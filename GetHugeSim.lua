589a206ab736d3477e7c649282c69befcd5d7188
Jimop1 <93406291+Jimop1@users.noreply.github.com>
2023-04-12 13:02:25 +0200

    Update GetHugeSim.lua

--- GetHugeSim.lua
+++ GetHugeSim.lua
@@ -32,7 +32,7 @@ local InfoTab = Window:MakeTab({
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
-InfoTab:AddParagraph("Updated",Last updated 12.4.2023")
+InfoTab:AddParagraph("Updated","Last updated 12.4.2023")
 InfoTab:AddParagraph("Changelogs", "+ Easter Event tab \nRemoved Leprechaun event tab \n=>Small fixes and changes")
 
 -- IslandTab
